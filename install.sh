27bfb89db17e0f0d268ca13eb57b71545461eb00
Piper Wolters <piperw@prior-cirrascale-64.reviz.ai2.in>
2024-08-30 15:08:08 -0700

    add wandb login to install

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ pip install -e ".[torch,metrics]"
 pip install wandb bitsandbytes openai
 pip install --upgrade huggingface_hub
 huggingface-cli login --token "$PIPERW_HF_TOKEN"
+wandb login "$PIPERW_WANDB_TOKEN"
 pip install -r requirements.txt
 python setup.py install
