0519f700c9433b6caefca7d36eb60795a2bed51c
Piper Wolters <piperw@prior-cirrascale-64.reviz.ai2.in>
2024-08-30 13:56:54 -0700

    install debugging

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ pip install wandb bitsandbytes openai
 pip install --upgrade huggingface_hub
 huggingface-cli login --token "$PIPERW_HF_TOKEN"
 pip install -r requirements.txt
-python setup.py
+python setup.py install
